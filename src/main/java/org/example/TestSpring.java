2f16a56ed790239944dc9a75026d0658e2ab8579
Данил <danila_monster@mail.ru>
2023-04-12 18:04:35 +0300

    commit-f

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,17 +8,21 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
         //Music music =  context.getBean("musicBean", Music.class);
 
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //musicPlayer.playMusicList();
 
         //вапвап
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
