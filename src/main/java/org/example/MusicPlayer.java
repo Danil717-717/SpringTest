8e37a7a2932709f39861373b14c15f41e471f491
Данил <danila_monster@mail.ru>
2023-04-12 19:14:48 +0300

    перед внедрением Аннотаций

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,25 +1,13 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
 
     private String name;
     private int volume;
-
-
-    public List<Music> getMusicList() {
-        return musicList;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-
 
     public String getName() {
         return name;
@@ -37,27 +25,19 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    //Ioc
-    //public MusicPlayer(Music music) {
-    //    this.music = music;
-    //}
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
 
-    public MusicPlayer(){}
+    public MusicPlayer() {}
 
-    //public void setMusic(Music music) {
-    //    this.music = music;
-    //}
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
-    //public void playMusic(){
-    //    System.out.println("Playing: "  + music.getSong());
-    //}
-
-    public void playMusicList(){
-        System.out.println("Start music");
-        for (Music music:musicList) {
-            System.out.println("Play: " + music.getSong());
-
-        }
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
     }
 
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -12,17 +12,6 @@ public class TestSpring {
 
         System.out.println(classicalMusic.getSong());
 
-        //Music music =  context.getBean("musicBean", Music.class);
-
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusicList();
-
-        //вапвап
-
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
